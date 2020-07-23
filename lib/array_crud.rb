eda7abc97f17af223d45cecea0a55e3e92cdd33b
Philip Warren <philwarren1@gmail.com>
2020-07-23 08:47:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,8 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
-  famous_cats[1]
+  array = ["wow", "hig", "am"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
