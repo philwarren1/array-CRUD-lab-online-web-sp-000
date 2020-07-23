91a5eece64ab7edc92840c67cdc39fffdffac5bc
Philip Warren <philwarren1@gmail.com>
2020-07-23 08:37:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
   array = ["h", "k"]
   element = "arrays!"
-  array << element
+  array.push(element)
 end
 
 def remove_element_from_end_of_array(array)
